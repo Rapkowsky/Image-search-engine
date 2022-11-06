3b2993b8c3b0b272eb708b2ca9f2f4be79444bac
“Robert <robert.rapkowski@tauceti.email>
2022-11-06 14:16:22 +0100

    - Load more photos functionality added

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const auth = "563492ad6f917000010000017dd21fe197d642f5985a0e4b22e2dc1a";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-form__inupt");
 const form = document.querySelector(".search-form");
-const more = document.querySelector(".nav-button__more");
-
+const moreBtn = document.querySelector(".nav-button__more");
 let searchValue;
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 // Listeners
 
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
+	currentSearch = searchValue;
 	searchPhotos(searchValue);
 });
+moreBtn.addEventListener("click", loadMore);
+
+// Fn
 
 function updateInput(e) {
 	searchValue = e.target.value;
@@ -46,17 +52,30 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-	const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
+	fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+	const data = await fetchApi(fetchLink);
 	generatePictures(data);
 }
 
 async function searchPhotos(inputValue) {
 	clearGallery();
-	const data = await fetchApi(`https://api.pexels.com/v1/search?query=${inputValue}+query&per_page=15&page=1`);
+	fetchLink = `https://api.pexels.com/v1/search?query=${inputValue}+query&per_page=15&page=1`;
+	const data = await fetchApi(fetchLink);
 	generatePictures(data);
 }
 function clearGallery() {
 	gallery.innerHTML = "";
 	searchInput.value = "";
+}
+
+async function loadMore() {
+	page++;
+	if (currentSearch) {
+		fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+	} else {
+		fetchLink = `https://api.pexels.com/v1/curated?per_page=15&${page}`;
+	}
+	const data = await fetchApi(fetchLink);
+	generatePictures(data);
 }
 curatedPhotos();
